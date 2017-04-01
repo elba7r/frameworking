51282eb55668644f2a2127d2e806e2cd43ea79cf
elba7r <elba7r@gmail.com>
2017-04-01 14:09:03 +0200

    icons-change

--- frappe/public/js/frappe/misc/user.js
+++ frappe/public/js/frappe/misc/user.js
@@ -38,7 +38,7 @@ frappe.ui.set_user_background = function(src, selector, style) {
 		}
 		var background = repl('background: url("%(src)s") center center;', {src: src});
 	} else {
-		var background = "background-color: #C4C4C4;";
+		var background = "background-color: #4D4D4D;";
 	}
 
 	frappe.dom.set_style(repl('%(selector)s { \
